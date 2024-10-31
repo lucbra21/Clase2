b2501e2f862f446f31adf7f7923c48626cb99593
Lucas Bracamonte <ing.lucasbracamonte@gmail.com>
2024-10-31 16:28:30 -0300

    trabajando con controles de entrada

--- app.py
+++ app.py
@@ -4,19 +4,25 @@ import pandas as pd
 from PIL import Image
 
 def main():
-    st.title("Trabajando con multimedia")
+    st.title("Trabajando con controles de ingreso de datos")
 
-    # img = Image.open("assets/imagen.png")
-    # img="https://sportsdatacampus.com/wp-content/uploads/2021/07/Sports-Data-Campus-Logo-e1625734322671.png"
-    # st.image(img, use_column_width=False)
-    # with open('assets/video.mp4','rb') as video_file:
-    #     st.video(video_file, start_time=0)
-    # with open('assets/audio.mp3','rb') as audio_file:
-    #      st.audio(audio_file, start_time=0)
+    nombre = st.text_input('Ingrese su nombre')
+    st.write(nombre)
+    
+    mensaje = st.text_area('Ingrese su nombre', height=100)
+    st.write(mensaje)
 
-    st.html(
-        "<p><span style='text-decoration: line-through double red;'>Oops</span>!</p>"
-    )
+    edad = st.number_input("Ingrese su edad",1,100,step=1)
+    st.write(edad)
+
+    fecha = st.date_input("Ingrese su fecha de nacimiento")
+    st.write(fecha)
+
+    hora = st.time_input("Ingrese la hora")
+    st.write(hora)
+
+    color = st.color_picker('seleccione un color')
+    st.write(color)
 
 if __name__ == "__main__":
     main()
