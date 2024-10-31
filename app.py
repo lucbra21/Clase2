ff0c32736d0b8c80776c52fb4947e4f2ff3865e2
Lucas Bracamonte <ing.lucasbracamonte@gmail.com>
2024-10-31 15:34:43 -0300

    mi primer aplicacion streamlit

---
+++ app.py
@@ -0,0 +1,10 @@
+# importar streamlit
+import streamlit as st 
+import pandas as pd
+from PIL import Image
+
+def main():
+    st.title("Mi primera app en Streamlit")
+
+if __name__ == "__main__":
+    main()
