82fa0ffdc321c1b4b837a17eaa837b364f09ff7c
Lucas Bracamonte <ing.lucasbracamonte@gmail.com>
2024-10-31 16:41:41 -0300

    personalizando configuraciones y sidebar

--- app.py
+++ app.py
@@ -3,26 +3,16 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
+img = Image.open('assets/imagen.png')
+st.set_page_config('MPAD M8', 
+                   page_icon=img, 
+                   layout='wide',
+                   initial_sidebar_state='collapsed')
+
 def main():
-    st.title("Trabajando con controles de ingreso de datos")
-
-    nombre = st.text_input('Ingrese su nombre')
-    st.write(nombre)
+    st.title("Personalizando mi aplicacion")
+    st.sidebar.header('Mi menú')
     
-    mensaje = st.text_area('Ingrese su nombre', height=100)
-    st.write(mensaje)
-
-    edad = st.number_input("Ingrese su edad",1,100,step=1)
-    st.write(edad)
-
-    fecha = st.date_input("Ingrese su fecha de nacimiento")
-    st.write(fecha)
-
-    hora = st.time_input("Ingrese la hora")
-    st.write(hora)
-
-    color = st.color_picker('seleccione un color')
-    st.write(color)
 
 if __name__ == "__main__":
     main()
